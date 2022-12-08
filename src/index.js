4e6c2d39985564d55363d968715034a4039c4b3f
collins kiprotich <collinskiprotich@gmail.com>
2022-12-08 07:19:47 +0300

    improved my to-do app styling

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import './style.css';
 
 document.addEventListener('DOMContentLoaded', () => {
   const locay = document.getElementById('todo');
-  const ul = document.createElement('ul');
+  const div = document.createElement('div');
+  div.classList.add('check-list');
   const todoItems = [
     { description: 'Prepare breakfast', completed: false, index: 0 },
     { description: 'Do laundry', completed: false, index: 1 },
@@ -13,9 +14,17 @@ document.addEventListener('DOMContentLoaded', () => {
   ];
   const itemArray = [];
   for (let i = 0; i < todoItems.length; i += 1) {
-    const listContent = `<li>${todoItems[i].description}</li>`;
+    const listContent = `<div class="flex-container">
+    <div class="left">
+    <input type="checkbox">
+    <div>${todoItems[i].description}</div>
+    </div>
+    <div class="dots"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-three-dots-vertical" viewBox="0 0 16 16">
+    <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"/>
+  </svg></div>
+</div>`;
     itemArray.push(listContent);
   }
-  ul.innerHTML = `${itemArray}`;
-  locay.appendChild(ul);
+  div.innerHTML = `${itemArray}`;
+  locay.appendChild(div);
 });
